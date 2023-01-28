030848c100d15dbc23271902cdd51234f71c515f
maks-pavlenkov <coolpavlenkov@mail.ru>
2023-01-28 18:22:54 +0300

    th

--- api_yamdb/users/validators.py
+++ api_yamdb/users/validators.py
@@ -6,13 +6,13 @@ from django.core.exceptions import ValidationError
 def validate_username(username):
     """Проверяет корректность поля username."""
 
-    PATTERN = re.compile(r'^[\w.@+-]+\Z')
+    pattern = re.compile(r'^[\w.@+-]+\Z')
 
     if username.lower() == 'me':
         raise ValidationError(
             message='Использовать имя "me" в качестве username запрещено. '
         )
-    if not PATTERN.findall(username):
+    if not pattern.findall(username):
         raise ValidationError(
             'Username содержит некорректные символы.'
         )
